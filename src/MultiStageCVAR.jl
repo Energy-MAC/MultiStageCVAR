e962e787c41b8eecf08940a998c3435abe08f437
Jose Daniel Lara <jdlara@berkeley.edu>
2021-03-15 19:32:32 -0700

    add methods file

---
+++ src/MultiStageCVAR.jl
@@ -0,0 +1,11 @@
+import PowerSystems
+import PowerSimulations
+import SDDP
+import DataFrames
+import Logging
+import LinearAlgebra
+import JSON
+import JuMP
+
+const PSI = PowerSimulations
+const PSY = PowerSystems
